809db2d33d8a3bded799e825cf4fac876373b2a9
omowunmi Elebute <omowunmi Elebute>
2017-03-13 09:47:33 -0400

    all test passed except 1

--- myP.js
+++ myP.js
@@ -1,3 +1,21 @@
 function getPrime(inpu){
-	
+	var arr=[]
+	var i;
+    for(i=2; i<=inpu; i++){
+    		if(check(i)==true){
+    		arr.push(i)
+    	
+    }
+}
+    	return arr;
+}
+function check( n) {
+    for( i=2;i<n;i++) {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+module.exports = {
+	getPrime: getPrime
 }
